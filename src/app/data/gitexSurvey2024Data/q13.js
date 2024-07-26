353a2221d22a8452904ba87ae57c596f9a83dfe6
rupesh6847 <rupeshmahajan516@gmail.com>
2024-07-27 01:40:08 +0530

    quetion 10 to 18

---
+++ src/app/data/gitexSurvey2024Data/q13.js
@@ -0,0 +1,4 @@
+export const q13 = {
+  question: "*Does your organisation have a strong security culture?",
+  answers: ["Yes", "No", "I don't know "],
+};
---
+++ src/app/data/gitexSurvey2024Data/q14.js
@@ -0,0 +1,10 @@
+export const q14 = {
+  question:
+    "*How would you describe your organisation’s readiness to deploy automation solutions?",
+  answers: [
+    "We have not yet deployed AI solutions but are preparing to",
+    "We are in the process of deploying AI solutions",
+    "We do not plan to deploy AI solutions ",
+    "I don’t know",
+  ],
+};
