3e372bd78efadbb1bcb4481f351fdf86cbc6ba1d
sushant <sushant.chougule@rmoperation.com>
2024-07-30 02:20:49 +0530

    new commi

--- src/app/components/pokerstars/main.jsx
+++ src/app/components/pokerstars/main.jsx
@@ -1,27 +1,19 @@
-// import React from "react";
-// import { useEffect } from "react";
-// import { useRecoilValue } from "recoil";
-// import { pokerStarsTab } from "@/app/atoms/pokerStarsTab";
-// import { ChooseCounty } from "./chooseCounty";
-// import { ChooseUsername } from "./chooseUsername";
-// import { ChoosePassword } from "./choosePassword";
-// import { ChooseEmail } from "./chooseEmail";
-import CenterMode from "./Carousel";
-import Carousel from "./Carousel";
+import React from "react";
+import { useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import { pokerStarsTab } from "@/app/atoms/pokerStarsTab";
+import { ChooseCounty } from "./chooseCounty";
 import { ChooseUsername } from "./chooseUsername";
-<<<<<<< HEAD
-import Hero from "./hero";
+import { ChoosePassword } from "./choosePassword";
+import { ChooseEmail } from "./chooseEmail";
 import "./main.css"
-import ValidateNameAndSurname from "./FirstNameLastName";
-// const Tab = ({ isActive, children }) =>
-//   isActive ? <div>{children}</div> : null;
-=======
+const Tab = ({ isActive, children }) =>
+  isActive ? <div>{children}</div> : null;
+
 import { ChoosePassword } from "./choosePassword";
 import { ChooseEmail } from "./chooseEmail";
 import { ChooseDate } from "./chooseDate";
-const Tab = ({ isActive, children }) =>
-  isActive ? <div>{children}</div> : null;
->>>>>>> 21bba3c23d4837da76f6d62858c3f7ed60690faf
+
 
 export const Main = () => {
   const tabNo = useRecoilValue(pokerStarsTab);
