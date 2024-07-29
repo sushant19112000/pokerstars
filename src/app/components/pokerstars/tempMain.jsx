d3a9197d94666567b2bb5c01d6052e79ef047eeb
sushant <sushant.chougule@rmoperation.com>
2024-07-30 00:06:45 +0530

    new commi

---
+++ src/app/components/pokerstars/tempMain.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import { pokerStarsTab } from "@/app/atoms/pokerStarsTab";
+import { ChooseCounty } from "./chooseCounty";
+import { ChooseUsername } from "./chooseUsername";
+import { ChoosePassword } from "./choosePassword";
+import { ChooseEmail } from "./chooseEmail";
+
+const Tab = ({ isActive, children }) =>
+  isActive ? <div>{children}</div> : null;
+
+export const Main = () => {
+  const tabNo = useRecoilValue(pokerStarsTab);
+  useEffect(() => {}, [tabNo]);
+  return (
+    <>
+      <div
+      >
+        <div>
+          <Tab isActive={tabNo === 1}>
+             <ChooseCounty/>
+          </Tab>
+          <Tab isActive={tabNo === 2}>
+            <ChooseUsername />
+          </Tab>
+          <Tab isActive={tabNo==3}>
+             <ChoosePassword/>
+          </Tab>
+          <Tab isActive={tabNo ===4}>
+             <ChooseEmail/>
+          </Tab>
+        </div>
+      </div>
+    </>
+  );
+};
