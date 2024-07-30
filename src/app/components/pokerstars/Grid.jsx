c8aeff763c5ff76dfb83bf8c5a077773f722ea6a
sushant <sushant.chougule@rmoperation.com>
2024-07-30 23:47:46 +0530

    new commi

---
+++ src/app/components/pokerstars/Grid.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Grid = () => {
+  return (
+    <div className='row justify-content-center'>
+        <div className="col-md-4 mb-2">
+            <picture>
+                <img className='img-fluid' alt="" />
+            </picture>
+        </div>
+        <div className="col-md-4 mb-2">
+            <picture>
+                <img className='img-fluid' alt="" />
+            </picture>
+        </div>
+        <div className="col-md-4 mb-2">
+            <picture>
+                <img className='img-fluid' alt="" />
+            </picture>
+        </div>
+    </div>
+ 
+)
+}
