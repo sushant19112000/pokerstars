36d8435f234c5dfd1ea31d28c1e3ded6d5f32f24
rupesh6847 <rupeshmahajan516@gmail.com>
2024-08-03 01:12:06 +0530

    form grid added

--- src/app/components/pokerstars/NewForm/form1.jsx
+++ src/app/components/pokerstars/NewForm/form1.jsx
@@ -5,7 +5,7 @@ import { countries } from "./countries.js";
 import { useNext } from "./useNext.jsx";
 import { useSaveMultipleData } from "./useSaveData.jsx";
 import Svg from "./svg.jsx";
-
+import "./form.css";
 const boldStyle = { fontWeight: "bold" };
 
 export const Form1 = () => {
@@ -14,9 +14,9 @@ export const Form1 = () => {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [dateofbirth, setDateOfBirth] = useState("");
+  const [countryofbirth, setCountryOfBirth] = useState("France");
   const [stateofbirth, setStateOfBirth] = useState("");
   const [cityofbirth, setCityOfBirth] = useState("");
-  const [address, setAddress] = useState("");
   const [firstname, setFirstName] = useState("");
   const [gender, setGender] = useState("");
   const [lastname, setLastName] = useState("");
@@ -36,11 +36,11 @@ export const Form1 = () => {
       setError("L'adresse e-mail n'est pas valide");
     }
   };
-console.log(country,"selected country")
+  console.log(country, "selected country");
   const next = useNext();
   const saveData = useSaveMultipleData();
   const handleNext = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     saveData({
       countryf1: country,
       username: username,
@@ -50,6 +50,9 @@ console.log(country,"selected country")
       firstname: firstname,
       lastname: lastname,
       gender: gender,
+      countryofbirth: countryofbirth,
+      stateofbirth: stateofbirth,
+      cityofbirth: cityofbirth,
     });
     next();
   };
@@ -80,13 +83,13 @@ console.log(country,"selected country")
                     <div className="col-lg-5 col-md-6 col-sm-12">
                       <div className="form-group mb-4">
                         <label style={boldStyle} htmlFor="inputEmail4">
-                        Dans quel pays habitez-vous ?
+                          Dans quel pays habitez-vous ?
                         </label>
                         <select
                           onChange={(e) => {
                             setCountry(e.target.value);
                           }}
-                          className="form-select"
+                          className="form-select form-input-border"
                           required
                         >
                           {countries.map((country, index) => (
@@ -96,12 +99,12 @@ console.log(country,"selected country")
                       </div>
                       <div className="form-group mb-4">
                         <label style={boldStyle} htmlFor="Mobile-Number">
-                        Mot de passe
+                          Mot de passe
                         </label>
                         <input
                           onChange={(e) => setPassword(e.target.value)}
                           type="text"
-                          className="form-control"
+                          className="form-control "
                           placeholder=""
                           required
                         />
@@ -122,7 +125,7 @@ console.log(country,"selected country")
                       </div>
                       <div className="form-group mb-4">
                         <label style={boldStyle} htmlFor="time">
-                        Date de naissance
+                          Date de naissance
                         </label>
                         <input
                           onChange={(e) => {
@@ -150,26 +153,12 @@ console.log(country,"selected country")
                           required
                         />
                       </div>
-                      {/* <div className="form-group mb-4">
-                        <label style={boldStyle} htmlFor="email">
-                          Adresse
-                        </label>
-                        <input
-                          onChange={(e) => {
-                            setAddress(e.target.value);
-                          }}
-                          type="text"
-                          className="form-control"
-                          id="text"
-                          placeholder=""
-                          required
-                        />
-                      </div> */}
                     </div>
+
                     <div className="col-lg-5 col-md-6 col-sm-12">
                       <div className="form-group mb-4">
                         <label style={boldStyle} htmlFor="last-name">
-                        Nom d'utilisateur
+                          Nom d'utilisateur
                         </label>
                         <input
                           onChange={(e) => setUsername(e.target.value)}
@@ -196,10 +185,10 @@ console.log(country,"selected country")
                       </div>
                       <div className="form-group mb-4">
                         <label style={boldStyle} htmlFor="email">
-                        E-mail
+                          E-mail
                         </label>
                         <input
-                          onChange={handleChange}
+                          onChange={(e) => setEmail(e.target.value)}
                           type="email"
                           className="form-control"
                           id="email"
@@ -221,24 +210,8 @@ console.log(country,"selected country")
                           </>
                         )} */}
                       </div>
-                      {/* <div className="form-group mb-4">
-                        <label style={boldStyle} htmlFor="birth-country">
-                          Pays de naissance
-                        </label>
-                        <select
-                          onChange={(e) => {
-                            setCountryOfBirth(e.target.value);
-                          }}
-                          className="form-select"
-                          required
-                        >
-                          {countries.map((country, index) => (
-                            <option key={index}>{country}</option>
-                          ))}
-                        </select>
-                      </div> */}
 
-<div className="form-group mb-4">
+                      <div className="form-group mb-4">
                         <label style={boldStyle} htmlFor="last-name">
                           Prénom
                         </label>
@@ -253,15 +226,6 @@ console.log(country,"selected country")
                           required
                         />
                       </div>
-
-
-
-
-
-
-
-
-
 
                       <div className="form-group mb-4">
                         <label style={boldStyle} htmlFor="email">
@@ -307,6 +271,72 @@ console.log(country,"selected country")
                             <span style={boldStyle} htmlFor="skill">
                               Femme
                             </span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      className="col-lg-10 col-md-10 col-sm-10"
+                      id="secondary-div-center"
+                    >
+                      <div>
+                        <h5
+                          style={{
+                            fontSize: "25px",
+                            color: "#D70022",
+                            fontWeight: "bolder",
+                          }}
+                          className="mb-4"
+                        >
+                          Où êtes-vous né(e) ?
+                        </h5>
+                        <div className="row justify-content-center justify-content-md-start">
+                          <div className="col-10 col-md-6 mb-3">
+                            <label style={boldStyle} htmlFor="birth-country">
+                              Pays de naissance
+                            </label>
+                            <select
+                              onChange={(e) => {
+                                setCountryOfBirth(e.target.value);
+                              }}
+                              className="form-select"
+                              required
+                            >
+                              {countries.map((country, index) => (
+                                <option key={index}>{country}</option>
+                              ))}
+                            </select>
+                          </div>
+
+                          <div className="col-10 col-md-6 mb-2">
+                            <label style={boldStyle} htmlFor="Mobile-Number">
+                              Département de naissance
+                            </label>
+                            <input
+                              onChange={(e) => {
+                                setStateOfBirth(e.target.value);
+                              }}
+                              type="text"
+                              className="form-control"
+                              id="Mobile-Number"
+                              placeholder=""
+                              required
+                            />
+                          </div>
+                          <div className="col-10 col-md-6">
+                            <label style={boldStyle} htmlFor="last-name">
+                              Ville de naissance
+                            </label>
+                            <input
+                              onChange={(e) => {
+                                setCityOfBirth(e.target.value);
+                              }}
+                              type="text"
+                              className="form-control"
+                              id="last-name"
+                              placeholder=""
+                              required
+                            />
                           </div>
                         </div>
                       </div>
--- src/app/components/pokerstars/NewForm/form2.jsx
+++ src/app/components/pokerstars/NewForm/form2.jsx
@@ -17,17 +17,14 @@ export const Form2 = () => {
   const [state, setState] = useState("");
   const [city, setCity] = useState("");
   const [postcode, setPostCode] = useState("");
-  const [firstname, setFirstName] = useState("");
-  const [lastname, setLastName] = useState("");
-  const [gender, setGender] = useState("");
+  
   const [sevendaylimit, setSevenDayLimit] = useState("");
   const [sevendayMinimumLimit, setSevendayMinimumLimit] = useState("");
   const [address, setAddress] = useState("");
   const [sevendayMaxmimumLimit, setSevendayMaxmimumLimit] = useState("");
   const [autoMaticWithdrawalLimit, setAutoMaticWithdrawalLimit] = useState("");
   const [loading, setLoading] = useState(false);
-  const [countryofbirth, setCountryOfBirth] = useState("France");
-  const pokerStarsData = useRecoilValue(PokerStarsData)
+  const pokerStarsData = useRecoilValue(PokerStarsData);
   const upload = useUpload();
   const prev = usePrevious();
   const saveData = useSaveMultipleData();
@@ -40,8 +37,7 @@ export const Form2 = () => {
   const handleNext = async (e) => {
     e.preventDefault();
     setLoading(true);
-    const dataOb =
-    {
+    const dataOb = {
       countryf1: pokerStarsData.countryf1,
       username: pokerStarsData.username,
       password: pokerStarsData.password,
@@ -54,33 +50,15 @@ export const Form2 = () => {
       state: state,
       city: city,
       postcode: postcode,
-      firstname: firstname,
-      lastname: lastname,
-      gender: gender,
+      firstname: pokerStarsData.firstname,
+      lastname: pokerStarsData.lastname,
+      gender: pokerStarsData.gender,
       sevenlimit: sevendaylimit.toString(),
       weeklylimit: sevendayMinimumLimit.toString(),
       depositlimit: sevendayMaxmimumLimit.toString(),
       autowithlimit: autoMaticWithdrawalLimit.toString(),
-    }
-    // countryf1:"",
-    // username:"",
-    // password:"",
-    // email:"",
-    // dob:"",
-    // gender:"",
-    // autowithlimit:"",
-    // stateofbirth:"",
-    // countryofbirth:"",
-    // cityofbirth:"",
-    // state:"",
-    // city:"",
-    // postcode:"",
-    // address:"",
-    // firstname:"",
-    // lastname:"",
-    // depositlimit:"",
-    // sevenlimit:"",
-    // weeklylimit:""
+    };
+
     console.log("upload data");
     // Upload data
     const uploadSuccess = await handleUpload(dataOb);
@@ -116,68 +94,23 @@ export const Form2 = () => {
 
                 <form onSubmit={handleNext}>
                   <div className="row justify-content-center justify-content-md-center">
-                    <div className="col-lg-10 col-md-10 col-sm-10" id="secondary-div-center">
-
-                      <div  style={{backgroundColor:'grey'}}>
-                        <h5 style={boldStyle} >Où êtes-vous né(e) ?</h5>
-                        <div className='row justify-content-center justify-content-md-start'>
-                          <div className="col-10 col-md-6 mb-2">
-                            <label style={boldStyle} htmlFor="birth-country">
-                              Pays de naissance
-                            </label>
-                            <select
-                              onChange={(e) => {
-                                setCountryOfBirth(e.target.value);
-                              }}
-                              className="form-select"
-                              required
-                            >
-                              {countries.map((country, index) => (
-                                <option key={index}>{country}</option>
-                              ))}
-                            </select>
-
-                          </div>
-
-                          <div className="col-10 col-md-6 mb-2">
-                            <label style={boldStyle} htmlFor="Mobile-Number">
-                              Département de naissance
-                            </label>
-                            <input
-                              onChange={(e) => {
-                                setState(e.target.value);
-                              }}
-                              type="text"
-                              className="form-control"
-                              id="Mobile-Number"
-                              placeholder=""
-                              required
-                            />
-
-                          </div>
+                    <div
+                      className="col-lg-10 col-md-10 col-sm-10"
+                      id="secondary-div-center"
+                    >
+                      <div className="">
+                        <h5
+                          style={{
+                            fontSize: "25px",
+                            color: "#D70022",
+                            fontWeight: "bolder",
+                          }}
+                          className="mb-4"
+                        >
+                          Où habitez-vous donc,?
+                        </h5>
+                        <div className="row justify-content-center justify-content-md-start">
                           <div className="col-10 col-md-6">
-                            <label style={boldStyle} htmlFor="last-name">
-                              Ville de naissance
-                            </label>
-                            <input
-                              onChange={(e) => {
-                                setCity(e.target.value);
-                              }}
-                              type="text"
-                              className="form-control"
-                              id="last-name"
-                              placeholder=""
-                              required
-                            />
-                          </div>
-                        </div>
-                      </div>
-                      <div className="mt-3" style={{backgroundColor:'grey'}}>
-                        <h5 style={boldStyle} >Où habitez-vous donc,?</h5>
-                        <div className='row justify-content-center justify-content-md-start'>
-
-                          <div className="col-10 col-md-6">
-
                             <label style={boldStyle} htmlFor="last-name">
                               Pays
                             </label>
@@ -191,8 +124,6 @@ export const Form2 = () => {
                               placeholder=""
                               required
                             />
-
-
                           </div>
 
                           <div className="col-10 col-md-6">
@@ -209,7 +140,6 @@ export const Form2 = () => {
                               placeholder=""
                               required
                             />
-
                           </div>
 
                           <div className="col-md-6">
@@ -262,10 +192,144 @@ export const Form2 = () => {
                         </div>
                       </div>
 
+                      <div className="mt-5">
+                        <h5
+                          style={{
+                            fontSize: "25px",
+                            color: "#D70022",
+                            fontWeight: "bolder",
+                          }}
+                          className="mb-4"
+                        >
+                          Limite de dépôt{" "}
+                        </h5>
+                        <div className="row justify-content-center justify-content-md-start">
+                          <div className="col-10 col-md-6">
+                            <label style={boldStyle} htmlFor="last-name">
+                              Limite de 7 jours
+                            </label>
+                            <input
+                              onChange={(e) => {
+                                setCity(e.target.value);
+                              }}
+                              type="text"
+                              className="form-control"
+                              id="last-name"
+                              placeholder=""
+                              required
+                            />
+                          </div>
+                        </div>
+                      </div>
 
+                      <div className="mt-5">
+                        <h5
+                          style={{
+                            fontSize: "25px",
+                            color: "#D70022",
+                            fontWeight: "bolder",
+                          }}
+                          className="mb-4"
+                        >
+                          Limite cumulée
+                        </h5>
+                        <div className="row justify-content-center justify-content-md-start">
+                          <div className="col-10 col-md-6">
+                            <label style={boldStyle} htmlFor="last-name">
+                              Limite de mise sur sept jours au poker
+                            </label>
+                            <input
+                              onChange={(e) => {
+                                setCity(e.target.value);
+                              }}
+                              type="text"
+                              className="form-control"
+                              id="last-name"
+                              placeholder=""
+                              required
+                            />
+                          </div>
+                          <div className="col-10 col-md-6">
+                            <label style={boldStyle} htmlFor="last-name">
+                              Limite de mises sur sept jours aux paris sportifs
+                            </label>
+                            <input
+                              onChange={(e) => {
+                                setCity(e.target.value);
+                              }}
+                              type="text"
+                              className="form-control"
+                              id="last-name"
+                              placeholder=""
+                              required
+                            />
+                          </div>
+                        </div>
+                      </div>
 
+                      <div className="mt-5">
+                        <h5
+                          style={{
+                            fontSize: "25px",
+                            color: "#D70022",
+                            fontWeight: "bolder",
+                          }}
+                          className="mb-4"
+                        >
+                          Limite de temps hebdomadaire au poker
+                        </h5>
+                        <div className="row justify-content-center justify-content-md-start">
+                          <div className="col-10 col-md-6">
+                            <label
+                              style={boldStyle}
+                              htmlFor="last-name"
+                            ></label>
+                            <input
+                              onChange={(e) => {
+                                setCity(e.target.value);
+                              }}
+                              type="text"
+                              className="form-control"
+                              id="last-name"
+                              placeholder=""
+                              required
+                            />
+                          </div>
+                        </div>
+                      </div>
 
-                      <div className="">
+                      <div className="mt-5">
+                        <h5
+                          style={{
+                            fontSize: "25px",
+                            color: "#D70022",
+                            fontWeight: "bolder",
+                          }}
+                          className="mb-4"
+                        >
+                          Fixer la limite de retrait automatique
+                        </h5>
+                        <div className="row justify-content-center justify-content-md-start">
+                          <div className="col-10 col-md-6">
+                            <label
+                              style={boldStyle}
+                              htmlFor="last-name"
+                            ></label>
+                            <input
+                              onChange={(e) => {
+                                setCity(e.target.value);
+                              }}
+                              type="text"
+                              className="form-control"
+                              id="last-name"
+                              placeholder=""
+                              required
+                            />
+                          </div>
+                        </div>
+                      </div>
+
+                      {/* <div className="">
                         <div className="form-group mb-4">
                           <label style={boldStyle} htmlFor="time">
                             Ville
@@ -313,24 +377,10 @@ export const Form2 = () => {
                             <option>300</option>
                           </select>
                         </div>
-                      </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                      </div> */}
 
                       <div className="col-lg-5 col-md-6 col-sm-12">
-                        <h5 style={boldStyle} >Où habitez-vous donc, jack ?</h5>
+                        {/*  <h5 style={{fontSize:"25px" ,color:"#D70022",fontWeight: "bolder"}}>Où habitez-vous donc, jack ?</h5>
                         <div className="form-group mb-4">
                           <label style={boldStyle} htmlFor="last-name">
                             Pays
@@ -345,9 +395,9 @@ export const Form2 = () => {
                             placeholder=""
                             required
                           />
-                        </div>
+                        </div> */}
 
-                        <div className="form-group mb-4">
+                        {/* <div className="form-group mb-4">
                           <label style={boldStyle} htmlFor="email">
                             Adresse
                           </label>
@@ -391,7 +441,7 @@ export const Form2 = () => {
                             placeholder=""
                             required
                           />
-                        </div>
+                        </div> */}
                         {/* <div className="form-group mb-4">
                         <label style={boldStyle} htmlFor="email">
                           Sexe
@@ -439,7 +489,7 @@ export const Form2 = () => {
                           </div>
                         </div>
                       </div> */}
-                        <div className="form-group mb-4">
+                        {/* <div className="form-group mb-4">
                           <label style={boldStyle} htmlFor="birth-country">
                             Limite de mise sur sept jours au poker
                           </label>
@@ -454,8 +504,8 @@ export const Form2 = () => {
                             <option>200</option>
                             <option>300</option>
                           </select>
-                        </div>
-                        <div className="form-group mb-4">
+                        </div> */}
+                        {/* <div className="form-group mb-4">
                           <label style={boldStyle} htmlFor="birth-city">
                             Limite de mises sur sept jours aux paris sportifs
                           </label>
@@ -470,18 +520,9 @@ export const Form2 = () => {
                             <option>200</option>
                             <option>300</option>
                           </select>
-                        </div>
+                        </div> */}
                       </div>
                     </div>
-
-
-
-
-
-
-
-
-
 
                     <div className="row justify-content-center py-3">
                       <div className="col-10 d-flex justify-content-between flex-wrap">
